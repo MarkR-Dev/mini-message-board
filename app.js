5919409c214f87ee10f1efab65877bd7f35abe70
Mark Rodgers <markrodgers.dev@gmail.com>
2025-10-01 15:56:38 +0100

    feat: setup server

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Index route");
+});
+
+// Matches paths that don't exists to produce a 404
+app.use("/{*splat}", (req, res) => {
+  res.status(404).send("404");
+});
+
+// Error handler middleware to catch errors throughout the app or previous middleware function if using next(err)
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(err.statusCode || 500).send(err.message);
+});
+
+// Setup express server instance to listen for requests
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, (err) => {
+  // This is important to display errors rather than silently failing
+  if (err) {
+    throw err;
+  }
+
+  console.log(`Server listening on port: ${PORT}`);
+});
